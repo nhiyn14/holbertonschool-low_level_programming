e575e74f2250f82599b7a6d2cec96732b6882bb9
Cienna Nguyen <4326@holbertonschool.com>
2022-02-03 23:59:21 +1100

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Print size of various types on the computer it is compiled and run on
+*
+* Description: Practice printf function
+*
+* Return: 0
+*/
+int main(void)
+{
+	printf();
+	return (0);
+}
