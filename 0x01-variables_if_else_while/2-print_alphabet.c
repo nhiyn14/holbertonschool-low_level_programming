00c05eee6e249c9d79fc2802e538d17c3c07a499
Cienna Nguyen <4326@holbertonschool.com>
2022-02-05 13:44:53 +1100

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Prints the alphabet in lowercase, followed by a new line
+*
+* Description: Output should be
+* abcdefghijklmnopqrstuvwxyz
+*
+* Return: 0
+*/
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	getchar();
+	return (0);
+}
