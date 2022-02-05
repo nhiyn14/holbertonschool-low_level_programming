bacbf9191ebc10b89e039ec993cc0a1c2da2e6ea
Cienna Nguyen <4326@holbertonschool.com>
2022-02-05 15:31:10 +1100

    Fix comments in 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - prints all the numbers of base 16 in lowercase
+* main - prints all single-digit numbers separated by ,
 *
 * Description: Output should be
-* 0123456789abcdef
+* 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 *
 * Return: 0
 */
