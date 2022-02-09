c2dc7cfbc426aff54db5ef87a60d3b40e8938a46
Cienna Nguyen <4326@holbertonschool.com>
2022-02-09 15:29:26 +1100

    Reorganize 0x02-functions_nested_loops dir

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * print_to_98 - rints all natural numbers from n to 98 \n
- * @n: number to start to bring from
+ * print_to_98 - prints all natural numbers from n to 98 \n
+ * @n: number to start to print from 98
  *
  * Description: if n = 98, print from 0 to 97
  * if n = 111, print from 110 to 99
  * if n = -10, print from -10 to 97
- *s
+ *
  * Return: Always 0.
  */
 void print_to_98(int n)
