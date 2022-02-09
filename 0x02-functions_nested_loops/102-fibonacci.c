ed06642ec47ca03176f92eb585f169eaa62ceeab
Cienna Nguyen <4326@holbertonschool.com>
2022-02-09 21:28:54 +1100

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2 \n
+  * @a: 1st number to create next Fibonacci number
+  * @b: 2nd number to create next Fibonacci number
+  * @c: count the number of addition (wanted: 50)
+  * Return: 0
+  */
+int main(void)
+{
+	double a = 1;
+	double b = 2;
+	double c = 0;
+
+	printf("%.0f, %.0f, ", a, b);
+	while (c < 50)
+	{
+		a = a + b;
+		printf("%.0f", a);
+		if (c < 49)
+		printf(", ");
+		c++;
+		b = a + b;
+		printf("%.0f", b);
+		if (c < 49)
+		printf(", ");
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
