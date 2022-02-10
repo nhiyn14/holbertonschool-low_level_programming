fccacecac944bbb404b3aaa3aad7cd78f1f48ae3
Cienna Nguyen <4326@holbertonschool.com>
2022-02-11 00:40:59 +1100

    Add 6-print_line.c and update main.h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line \n
+ * @n: number of times the character _ should be printed
+ * Description: output:
+ * if n = 10__________
+ *
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	}
+	_putchar('\n');
+}
