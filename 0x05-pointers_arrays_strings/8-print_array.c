190005d905259d197398937d0084faa8ed8a11cc
Cienna Nguyen <4326@holbertonschool.com>
2022-02-14 14:16:56 +1100

    Add 8-print_arracy.c and update main.h

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers /n
+ * @a: pointer a (set to int array)
+ * @n: number of element
+ *
+ *
+ * Description: if array = 1, 2, 3
+ * => Stdout: 1, 2, 3
+ *
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	printf("%d", a[0]);
+	for (b = 1; b < n; b++)
+		printf(", %d", a[b]);
+	printf("\n");
+}
