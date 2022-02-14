257e43696c97debde33ffd49205f2c552c826996
Cienna Nguyen <4326@holbertonschool.com>
2022-02-14 11:17:13 +1100

    Create 0x05 dir, README.md, 0-reset_to_98.c and main.h file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  use a pointer updates the value it points to to 98
+ * @n: pointer n
+ *
+ * Description: output:
+ * n = 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+200~0x05-pointers_arrays_strings directory was created for 0x05. C - Pointers, arrays and strings project
+Holberton Week4
