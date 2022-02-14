7e989fd025a401f3e30d94f9ff03306245efd78c
Cienna Nguyen <4326@holbertonschool.com>
2022-02-14 13:32:33 +1100

    Add 6-puts2.c and update main.h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * puts2 - every other character of a string /n
+ * @str: pointer str (set to int str)
+ *
+ *
+ * Description: output: int str = "Hello"
+ * => Stdout: Hlo
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int len = _strlen(str);
+	int pre = 0;
+
+	while (pre <= len)
+	{
+		if (pre % 2 == 0)
+			_putchar(str[pre]);
+		pre++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - print length of string
+ * @str: pointer s
+ * Return: 0
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
