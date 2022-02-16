d65efda1f87990653550a5d35d81e633830b37c8
Cienna Nguyen <4326@holbertonschool.com>
2022-02-16 21:11:47 +1100

    Add 0x06-pointers dir, README.md, 0-strcat.c and main.h file

---
+++ 0x06-pointers_array_strings/README.md
@@ -0,0 +1,2 @@
+0x06-pointers_arrays_strings directory was created for 0x05. C - More pointers, arrays and strings
+Holberton Week 3
---
+++ 0x06-pointers_array_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _strlen(char *src);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
