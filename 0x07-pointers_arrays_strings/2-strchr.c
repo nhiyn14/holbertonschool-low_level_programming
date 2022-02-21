48a9e539315204a9e7a9c4db6da36d05479aa81c
Cienna Nguyen <4326@holbertonschool.com>
2022-02-21 21:48:29 +1100

    Add 2-strchr.c and update main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s: string to check
+ * @c: character to locate
+ *
+ * Description: Similar to functiob strchr in stdio.h
+ *
+ * Return: 0 if no match, s if match
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		while (s[i] != '\0')
+		{
+			if (s[i] == c)
+			{
+				s = &s[i];
+			}
+			else
+				i++;
+		}
+		return (s);
+	}
+	else
+		return (0);
+}
