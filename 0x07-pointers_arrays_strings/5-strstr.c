a51871da34b3594fa2d92c0ab11b82d28d7d71a3
Cienna Nguyen <4326@holbertonschool.com>
2022-02-22 00:05:04 +1100

    Add 5-strstr.c and update main.h

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - a function that locates a substring.
+ *
+ * @haystack: string to check
+ * @needle: string to check against
+ *
+ * Description: Similar to functiob strstr in stdio.h
+ *
+ * Return:byte first occurrence of needle in haystack
+ * 0 if no such byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0; /**placeholder of haystack */
+	int j = 0; /**placeholder of needle */
+	int temp = i;
+	int hayLen = _strlen(haystack);
+	int neeLen = _strlen(needle);
+
+	for (i = 0; i < hayLen; i++)
+	{
+		temp = i;
+		for (j = 0; j < neeLen; j++)
+		{
+			if (haystack[temp] == needle[j])
+			{
+				if (j == neeLen - 1)
+					return (haystack + i);
+				temp++;
+			}
+			else
+				break;
+		}
+	}
+	return (0);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer s (set to int a)
+ *
+ *
+ * Description: output: str = Hello
+ * _strlen(str) = 5
+ *
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
