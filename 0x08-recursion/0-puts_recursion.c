20f989480f7a74e930a7334492b34232981b6719
Cienna Nguyen <4326@holbertonschool.com>
2022-02-23 10:50:12 +1100

    Fix incorrect code when input is NULL

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,14 +10,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(*(s + i));
-	i++;
-	if (s[i] != '\0')
-	{
-		_puts_recursion(s + i);
-	}
-	else
+	if (*s == '\0')
 		_putchar('\n');
+	
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+		
 }
