85d20ff2e5150a98d41d73dd0706a96f6a057bbc
Cienna Nguyen <4326@holbertonschool.com>
2022-02-24 13:16:32 +1100

    Remove unnecessary if condition]

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,10 +14,8 @@ int is_prime_number(int n)
 {
 	int x = 2;
 
-	if (n <= 3)
+	if (n <= 1)
 		return (0);
-	else if (n == 0 || n == 1)
-		return (n);
 	else
 		return (_manualProductCheck(x, n));
 }
