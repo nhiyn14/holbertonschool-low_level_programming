4c6881feb8706a2d5f77d7976bb1f9d6f4042a36
Cienna Nguyen <4326@holbertonschool.com>
2022-02-27 15:12:38 +1100

    Add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - program that multiplies two numbers \n
+  * @argc: argument count
+  * @argv: array of pointer
+  *
+  * Description:  stdout is product of input 2 numbers
+  *
+  * Return: Product if enter 2 numbers
+  * else return Error
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error\n");
+	return (0);
+}
