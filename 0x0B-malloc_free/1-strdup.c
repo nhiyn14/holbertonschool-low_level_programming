89e1a3ed3236dfddab61e64ec311c311e135a922
Cienna Nguyen <4326@holbertonschool.com>
2022-03-01 11:44:18 +1100

    Add 1-strdup.c and update main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * *_strdup- returns a pointer to a newly allocated space in memory,
+  * which contains a copy of the string given as a parameter.
+  * @str: given string
+  *
+  * Description: Similar to function strdup
+  *
+  * Return: if succeed, return pointer to the duplicate of str
+  * NULL if str == NULL or insufficient memory was available
+  */
+char *_strdup(char *str)
+{
+	char *arrayChar;
+	char size = _strlen(str);
+	int i = 0; /*place holder of arrayChar*/
+
+	arrayChar = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		*(arrayChar + i) = *(str + i);
+		i = i + 1;
+	}
+	return (arrayChar);
+	free(arrayChar);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @str: pointer s (set to int a)
+ *
+ *
+ * Description: output: str = Hello
+ * _strlen(str) = 5
+ *
+ * Return: Length
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
