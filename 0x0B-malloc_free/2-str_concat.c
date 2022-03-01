95181e7252d0ed6fc56fb0b51f96abbe830bbc31
Cienna Nguyen <4326@holbertonschool.com>
2022-03-01 16:40:38 +1100

    Remove _strdup function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -40,36 +40,6 @@ char *str_concat(char *s1, char *s2)
 }
 
 /**
-  * *_strdup- returns a pointer to a newly allocated space in memory,
-  * which contains a copy of the string given as a parameter.
-  * @str: given string
-  *
-  * Description: Similar to function strdup
-  *
-  * Return: if succeed, return pointer to the duplicate of str
-  * NULL if str == NULL or insufficient memory was available
-  */
-char *_strdup(char *str)
-{
-	char *arrayChar;
-	int size; /* +1 for \0 */
-	int i = 0; /*place holder of arrayChar*/
-
-	if (str == NULL)
-		return (NULL);
-	size = _strlen(str) + 1;
-	arrayChar = malloc(size);
-	if (arrayChar == NULL)
-		return (NULL);
-	while (i < size)
-	{
-		*(arrayChar + i) = *(str + i);
-		i = i + 1;
-	}
-	return (arrayChar);
-}
-
-/**
  * _strlen - returns the length of a string.
  * @str: pointer s (set to int a)
  *
