38e77cbee374995f7d5b65edb77b77c9b53a361f
Cienna Nguyen <4326@holbertonschool.com>
2022-03-01 16:14:32 +1100

    Add 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * *str_concat - a function that concatenates two strings.
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Description: if s1 = "Best ", s2 = "Pie"
+  * Stdout: Best School
+  *
+  * Return: the wanted array or NULL if fail or size = 0
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int sizeS1, sizeS2, sizeCon;
+	int i = 0; /*place holder of concat*/
+	int a = 0; /*place holder of s2*/
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	sizeS1 = _strlen(s1);
+	sizeS2 = _strlen(s2) + 1; /* +1 to include \0 */
+	sizeCon = sizeS1 + sizeS2;
+	concat = malloc(sizeCon);
+	if (concat == NULL)
+		return (NULL);
+	while (i < sizeS1)
+	{
+		*(concat + i) = *(s1 + i);
+		i = i + 1;
+	}
+	while (i < sizeCon)
+	{
+		*(concat + i) = *(s2 + a);
+		i++;
+		a++;
+	}
+	return (concat);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @str: pointer s (set to int a)
+ *
+ *
+ * Description: output: str = Hello
+ * _strlen(str) = 5
+ *
+ * Return: Length
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
