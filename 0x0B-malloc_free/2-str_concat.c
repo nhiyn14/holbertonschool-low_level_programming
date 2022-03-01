f5d3ccc2958cc1cf85f635ce3f78a51db7f3dd3c
Cienna Nguyen <4326@holbertonschool.com>
2022-03-01 16:37:55 +1100

    Fix code to change stdout when both s1 and s2 is NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,8 +19,6 @@ char *str_concat(char *s1, char *s2)
 	int i = 0; /*place holder of concat*/
 	int a = 0; /*place holder of s2*/
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL && s1 != NULL)
