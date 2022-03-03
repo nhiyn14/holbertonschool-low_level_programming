4412639168ece6ff46a83dfd164b30a024071038
Cienna Nguyen <4326@holbertonschool.com>
2022-03-03 22:04:34 +1100

    Fix malloc condition

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 && size == 0)
 		return (NULL);
-	calloc = malloc(sizeof(size) * nmemb);
+	calloc = malloc(size * nmemb);
 	if (calloc == NULL)
 	{
 		free(calloc);
