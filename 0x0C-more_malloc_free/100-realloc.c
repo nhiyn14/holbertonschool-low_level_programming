718d897d53c6ce74220a4767ebb7742e884c592d
Cienna Nguyen <4326@holbertonschool.com>
2022-03-03 23:22:43 +1100

    Fix return when realloc == NULL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,7 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (realloc == NULL)
 	{
 		free(realloc);
-		return (realloc);
+		return (NULL);
 	}
 	for (i = 0; i < old_size; i++)
 		*(realloc + i) = *(cast + i);
