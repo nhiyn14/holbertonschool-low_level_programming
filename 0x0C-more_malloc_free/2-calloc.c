ce3100c89bbf4b0278f00acd0c4df9a9f9399846
Cienna Nguyen <4326@holbertonschool.com>
2022-03-03 22:11:37 +1100

    change && to ||

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *calloc;
 	unsigned int i = 0; /*place holder of calloc*/
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	calloc = malloc(size * nmemb);
 	if (calloc == NULL)
