8a719a4a375d4309a097704fcee485d80e39a4e0
Cienna Nguyen <4326@holbertonschool.com>
2022-03-07 12:15:15 +1100

    Add 0x0E-structures_typedef dir, README.md, dog.h file and compiled file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+0x0E-structures_typedef directory was created  
+for 0x0E. C - Structures, typedef project  
+Holberton School Aus  
+Date: 07/03/2022  
+  
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+  * struct dog - details of dog
+  * @name: doggo name
+  * @age: doggo age
+  * @owner: doggo human friend
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
