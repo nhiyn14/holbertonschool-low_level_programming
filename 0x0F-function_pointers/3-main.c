a3b5158c98afd59a791000882286060e58ac5ca7
Cienna Nguyen <4326@holbertonschool.com>
2022-03-10 01:21:48 +1100

    Sudo-ish code, still wrong stdout

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,8 +4,35 @@
 /**
   * main - a program that performs simple operations
   *
-  * Return:
+  * Return: Wrong number of arguments, print Error\n, exit 98
+  * Operator not match, print Error\n, exit 99
+  * Divide by 0, print Error\n, exit 100
   */
-int main()
+int main(int argc, char **argv)
 {
+	int a, b, result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+ 	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	result = func(a, b);
+	printf("%d\n", result);
+	return(0);
 }
