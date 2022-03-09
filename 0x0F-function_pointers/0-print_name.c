5935256f4f117235fbd6995975c9fac64249d974
Cienna Nguyen <4326@holbertonschool.com>
2022-03-09 20:45:48 +1100

    Add if name/function is NULL condition

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
-  * print_name: function that prints a name
+  * print_name - function that prints a name
   * @name: name to be printed
   * @f: function print
   *
@@ -13,5 +13,6 @@ void print_name(char *name, void (*f)(char *))
 	void (*p)(char *);
 
 	p = f;
-	p(name);
+	if (name != NULL && p != NULL)
+		p(name);
 }
