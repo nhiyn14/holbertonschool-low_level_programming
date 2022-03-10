69eda84b10e9ed23a1763224b05fa8a59fe7359a
Cienna Nguyen <4326@holbertonschool.com>
2022-03-10 14:06:41 +1100

    Add 2-print_strings.c and compiled file

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 4, NULL, "Django", "cIEN", "KITKAT");
+    return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+  * print_string -  a function that prints strings \n
+  * @separator: the string to be printed between numbers
+  * @n: number of strings passed to the func
+  *
+  * Return: if separator == NULL, print nothing
+  * if string == NULL,  print (nil)
+  * end with \n
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strList;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(strList, n); /* Save arguments in list */
+	while (i < n)
+	{
+		str = va_arg(strList, char*);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(strList);
+}
