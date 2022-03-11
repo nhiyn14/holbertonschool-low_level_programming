d4aef0ebc16a207952deaf23edc13b4206573db3
Cienna Nguyen <4326@holbertonschool.com>
2022-03-11 22:37:27 +1100

    Add 3-print_all.c and compiled file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,64 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+  * print_all -  a function that prints anything \n
+  * @format: list of accepted types of arguments passed to the func
+  *
+  * Return: up to 9 variables
+  * if separator == NULL, print nothing
+  * if string == NULL,  print (nil)
+  * end with \n
+  */
+void print_all(const char * const format, ...)
+{
+	va_list type;
+	unsigned int i = 0;
+	char *str;
+	char *separator = "";
+
+	if (format != NULL)
+	{
+		va_start(type, format); /* first call to check format */
+		while (format[i] != '\0')
+		{	
+			switch (format[i])
+			{
+				case 'c':
+				{
+					printf("%s%c", separator, va_arg(type, int));
+					separator = ", ";
+					break;
+				}
+				case 'i':
+				{
+					printf("%s%i", separator, va_arg(type, int));
+					separator = ", ";
+					break;
+				}
+				case 'f':
+				{
+					printf("%s%f", separator, va_arg(type, double));
+					separator = ", ";
+					break;
+				}
+				case 's':
+				{	
+					str = va_arg(type, char *);
+					if (str != NULL)
+					{
+						printf("%s%s", separator, str);
+						separator = ", ";
+						break;
+					}
+					printf("(nil)");
+					separator = ", ";
+					break;
+				}
+			}
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(type);
+}
