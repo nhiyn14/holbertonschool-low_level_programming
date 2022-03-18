f3106d2d6eb7f34da93ffcf53579d36d638ce4c6
Cienna Nguyen <4326@holbertonschool.com>
2022-03-18 13:56:02 +1100

    Add if str == NULL

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	list_t *last = *head;
 
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node->str);
+		return (NULL);
+	}
+
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 	if (*head == NULL)
@@ -42,6 +53,9 @@ list_t *add_node_end(list_t **head, const char *str)
 int _strlen(const char *s)
 {
 	unsigned int count = 0;
+
+	if (s == NULL)
+		return (0);
 
 	while (*s != '\0')
 	{
