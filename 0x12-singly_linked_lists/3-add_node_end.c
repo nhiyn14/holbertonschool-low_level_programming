1b72a1e07ef300d0c1eec813007af1d3cf58be1e
Cienna Nguyen <4326@holbertonschool.com>
2022-03-18 13:57:45 +1100

    Change free new_node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,7 +22,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
 
