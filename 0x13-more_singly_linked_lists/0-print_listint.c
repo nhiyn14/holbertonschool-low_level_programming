a1544d155cbcdca54b3fa3a132ec642c619078f1
Cienna Nguyen <4326@holbertonschool.com>
2022-03-21 09:56:58 +1100

    Add 0x13-more_singly_linked_lists dir, README.md, lists.h and 0-print_listint.c and its compiled file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * print_listint - prints all the elements of a listint_t list.
+  * @h: linked list to print
+  * Return: the number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+0x13-more_singly_linked_lists directory was created  
+for 0x13. C - More singly linked lists project  
+Holberton School Aus  
+Date: 21/03/2022  
+  
+
