5908e7c92dcc72fe27568e89987855d1737022fd
Cienna Nguyen <4326@holbertonschool.com>
2022-03-22 10:24:11 +1100

    Fix to betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
 /**
   * get_nodeint_at_index - returns the nth node of a linked list.
   * @head: pointer to head of list.
-  * index: the index of the node, starting at 0
+  * @index: the index of the node, starting at 0
   * Return: NULL if node does not exist
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
