41a6559d4416b1ebfea1eb9933803108b9a937e4
Cienna Nguyen <4326@holbertonschool.com>
2022-03-22 09:15:58 +1100

    Fix while condition and remove free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		tmp = head->next;
 		free(head);
 		head = tmp;
 	}
-	free(head);
 }
