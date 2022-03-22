4c2802d14ea3b67cad690dd4ca784b9e1c245cdb
Cienna Nguyen <4326@holbertonschool.com>
2022-03-22 14:31:02 +1100

    Correct the locatin of newNode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (tmp != NULL)
 		{
-			if (count == idx)
+			if (count == (idx-1))
 			{
 				newNode->next = tmp->next;
 				tmp->next = newNode;
