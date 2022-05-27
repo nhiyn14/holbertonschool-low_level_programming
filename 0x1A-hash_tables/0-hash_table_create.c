6601f6251ab30d174b39e0acc82861bee7cd8c21
Cienna Nguyen <4326@holbertonschool.com>
2022-05-27 15:27:24 +1000

    Corrct malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	htable->size = size;
-	htable->array = malloc(sizeof(hash_table_t *) * size);
+	htable->array = malloc(sizeof(hash_node_t *) * size);
 	if (htable->array == NULL)
 	{
 		free(htable);
